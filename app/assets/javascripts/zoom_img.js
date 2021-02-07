6063cf47c52621e0ffa9f2af0d5ad772401e2ba1
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-02-07 10:36:33 +0000

    tag partial

---
+++ app/assets/javascripts/zoom_img.js
@@ -0,0 +1,25 @@
+$(function() {
+
+const imgModal = $('<div id="img_modal">');
+const bgArea = $('<div id="img_modal_bg">');
+const imgArea = $('<div id="img_modal_body">');
+const zoomImg = $('<img id="zoom_img">');
+    
+    $('body').append(imgModal);
+    imgModal.append(bgArea,imgArea);
+    imgArea.append(zoomImg);
+    imgModal.remove();
+    
+
+$(".main_img_area img").click(function(){
+    const thisImg = $(this).attr('src');
+    zoomImg.attr({src:thisImg});
+    $('body').append(imgModal);
+    
+    $(bgArea).click(function(){
+       imgModal.remove(); 
+    });
+});
+
+    
+});
