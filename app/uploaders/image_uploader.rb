cdf03ae09a1609eb3c84d5548119aa7b6e211cfc
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-02-10 06:39:02 +0000

    before imagemagick install

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,6 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     true # nothing, the dir is not empty
     end
 
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
