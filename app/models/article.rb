0f5c64b1d934373ebe7b415b81730d2c75bc2793
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-02-13 09:32:18 +0000

    font awesome partial remake

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
 
-
+# show画面で前後の投稿を取得
 
   def previous
     user.articles.order('created_at desc, id desc').where('created_at <= ? and id < ?', created_at, id).first
@@ -11,6 +11,16 @@ class Article < ApplicationRecord
 
   def next
     user.articles.order('created_at desc, id desc').where('created_at >= ? and id > ?', created_at, id).reverse.first
+  end
+  
+  # 同じタグに属するレコードの前後を取得
+  
+  def prev_tag
+    user.tag.articles.order('created_at desc, id desc').where('created_at <= ? and id < ?', created_at, id).first
+  end
+
+  def next_tag
+    user.tag.articles.order('created_at desc, id desc').where('created_at >= ? and id > ?', created_at, id).reverse.first
   end
   
 end
