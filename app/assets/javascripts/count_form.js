2a4fae53e9045bcb2ff25c0465a0a9743ac99498
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-02-16 07:07:06 +0000

    count_form.js done

--- app/assets/javascripts/count_form.js
+++ app/assets/javascripts/count_form.js
@@ -1,22 +1,31 @@
 $(function(){
     
-    const hoge = $('#input_text');
-    const piyo = $('#counter');
-    const mofu = $('#limit');
-    const maxLength = mofu.data("count");
-    let countNum = String(hoge.val().length);
-    piyo.text(countNum + "文字");
-    mofu.text(maxLength + "文字");
-    
- hoge.on("keyup", function() {
-   let countNum = String($(this).val().length);
-      // 現在の文字数と最大文字数を比較して、
-   // 現在の文字数が多ければ赤く表示する
-    if ( parseInt(countNum) > parseInt(maxLength) ) {
-      piyo.text(countNum + "文字").css('color','red');
-    } else {
-        piyo.text(countNum + "文字").css('color','inherit');
+    function countInputText(input) {
+        const piyo = $(input).next('p').find('#counter');
+        const mofu = $(input).next('p').find('#limit');
+        const maxLength = mofu.data("count");
+        let countNum = String($(input).val().length);
+        piyo.text(countNum + "文字");
+        mofu.text(maxLength + "文字");
     }
- });
+    
+      // ページ表示時に文字数をカウントして表示
+  $("input#input_text,textarea#input_text").each(function(){
+    countInputText($(this));
+  });
+  
+    // フォーム入力時に文字数をカウントして表示
+  $("input#input_text,textarea#input_text").on('keyup',function(){
+       let countNum = String($(this).val().length);
+       const thisCounter = $(this).next('p').find('#counter');
+       const thisLimit = thisCounter.next('span#limit').data('count');
+     // 現在の文字数と最大文字数を比較して、
+   // 現在の文字数が多ければ赤く表示する
+    if ( parseInt(countNum) > parseInt(thisLimit) ) {
+      thisCounter.text(countNum + "文字").css('color','red');
+    } else {
+        thisCounter.text(countNum + "文字").css('color','inherit');
+    }
+  });
  
 });
