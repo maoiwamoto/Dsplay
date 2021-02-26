844eb125d20cdbb820dcdebb6235e1db1949ae16
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-02-26 17:48:33 +0000

    mypage link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   
+  before_action :authenticate_user!, except: [:show]
+  
   def mypage
-      @user = current_user
   end
   
   def show
       @user = User.find_by(id: params[:id])
-      redirect_to(home_path, alert: "ERROR!!　指定したIDのユーザーは存在しません") if @user.nil?
+      redirect_to(home_path, alert: "ERROR!!") if @user.nil?
   end
   
 end
