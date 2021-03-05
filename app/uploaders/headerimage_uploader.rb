0af8f09b3f6b2a0481c5940eceeaf5121045df80
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-03-05 14:21:13 +0000

    tag index break point change

--- app/uploaders/headerimage_uploader.rb
+++ app/uploaders/headerimage_uploader.rb
@@ -5,7 +5,7 @@ process :convert => 'jpg' # 画像の保存形式
 process :quality => 'auto'
 process :tags => ['user_headerimage']
 cloudinary_transformation :transformation => [
-        {:width => 700, :height => 450, :crop => "limit"}]
+        {:width => 700, :height => 500, :crop => "limit"}]
   
   def size_range
   0..2.megabytes
