395e838bcb57b72c7629c28f5b947567a1364080
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-03-07 14:03:33 +0000

    env set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,8 @@ Rails.application.configure do
      host: 'dsplay.herokuapp.com'
    }
    # mail setting
-   config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.perform_deliveries = true
+   config.action_mailer.raise_delivery_errors = false
    config.action_mailer.delivery_method = :smtp
    config.action_mailer.smtp_settings = {
      :address => "smtp.gmail.com",
