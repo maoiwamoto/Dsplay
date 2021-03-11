eee97e83fcab27798800ea787976cbfe005c7371
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-03-11 10:26:19 +0000

    slash rm host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://dsplay.herokuapp.com/'
+  host = 'https://dsplay.herokuapp.com'
   config.action_mailer.default_url_options = { protocol: 'https', host: host }
   ActionMailer::Base.smtp_settings = {
      :port => ENV["MAILGUN_SMTP_PORT"],
