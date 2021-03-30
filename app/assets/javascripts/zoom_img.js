c27803665276f9661261d910cb5e514ab6b61fca
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-03-30 10:21:43 +0000

    make close button modal

--- app/assets/javascripts/zoom_img.js
+++ app/assets/javascripts/zoom_img.js
@@ -4,23 +4,25 @@ const imgModal = $('<div id="img_modal">');
 const bgArea = $('<div id="img_modal_bg">');
 const imgArea = $('<div id="img_modal_body">');
 const zoomImg = $('<img id="zoom_img">');
+const closeIcon = $('<span id="close_icon"></span>');
     
 
     $('body').append(imgModal);
     imgModal.append(bgArea,imgArea);
     imgArea.append(zoomImg);
+    imgModal.append(closeIcon);
     imgModal.remove();
-
-
 
 $(".main_img_area img").click(function(){
     const thisImg = $(this).attr('src');
     zoomImg.attr({src:thisImg});
     $('body').append(imgModal);
     
-    $(bgArea).click(function(){
+    bgArea.add(closeIcon).click(function(){
        imgModal.remove(); 
     });
+    
+    
     });
 
     
