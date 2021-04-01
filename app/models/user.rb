12a150be8b097fb758505eb864fece7c4e8ef07b
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-04-01 10:30:59 +0000

    user model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable #←confirmableを追加
+         :recoverable, :rememberable, :validatable, :confirmable, :timeoutable, :lockable
+         
+  validate :password_complexity
+def password_complexity
+  　　　　　return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+  errors.add :password, "パスワードの強度が不足しています。パスワードの長さは8〜70文字とし、大文字と小文字と数字と特殊文字をそれぞれ1文字以上含める必要があります。"
+end
          
 def tags
 return Tag.where(user_id: self.id)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_102826) do
+ActiveRecord::Schema.define(version: 2021_04_01_073141) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -85,8 +85,12 @@ ActiveRecord::Schema.define(version: 2021_03_29_102826) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
