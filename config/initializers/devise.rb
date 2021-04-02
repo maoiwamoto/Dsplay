0f7c0e231facad4d9bf9aa53cbf13e024fb95dee
maoiwamoto <qqnx8wgd@theia.ocn.ne.jp>
2021-04-02 10:40:17 +0000

    devise image

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -215,7 +215,7 @@ Devise.setup do |config|
   config.maximum_attempts = 10
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
-  config.unlock_in = 1.hour
+  #config.unlock_in = 1.hour
 
   # Warn on the last attempt before the account is locked.
   config.last_attempt_warning = false
